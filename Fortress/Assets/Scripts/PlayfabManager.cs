4a18ea1773416bba8b0b46a1d9ef80a10005a7bb
smnnmn <leejs899797@gmail.com>
2025-03-14 21:44:57 +0900

    {2025.3.14} 데이터베이스 설치

---
+++ Fortress/Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using PlayFab;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Succeed,
+            Fail
+        );
+
+    }
+
+    public void Succeed(LoginResult loginResult)
+    {
+        // 마스터 클라이언트가 씬을 이동할때 다른 remote클라이언트들이
+        // 같이 이동될지 안될지
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Lobby");
+    }
+
+    public void Fail(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.ToString());
+    }
+
+}
